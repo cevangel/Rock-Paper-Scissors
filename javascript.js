c816c612c0a5f4c436d5c2c864ebd402f101d365
christopher <cevangdpt@gmail.com>
2023-03-29 00:45:04 -0400

    Add loop to game function in order to have it play 5 rounds

--- javascript.js
+++ javascript.js
@@ -1,13 +1,3 @@
-console.log("Hello World!");
-
-
-//1. getComputerChoice
-  //Math.floor(Math.random() * 3) + 1;
-  //if 1 then CC is Rock
-  //2 '' Paper
-  //3 '' Scissors
-
-const computerSelection = getComputerChoice();
 function getComputerChoice(){  
   switch(Math.floor(Math.random() * 3) + 1) {
     case 1:
@@ -21,32 +11,9 @@ function getComputerChoice(){
       break;
     }
   }
-  console.log(computerSelection)
-//2. Get playerSelection: prompt user "choose Rock paper or scissors"
-  //use prompt()
-  //make case insensitive
-
-let playerString = prompt("Choose Rock, Paper or Scissors","")
-const playerSelection = playerString[0].toUpperCase() + playerString.slice(1).toLowerCase();
-console.log(playerSelection)
-
-
-//3. function playRound(playerSelection, computerSelection){}
-  //switch statement of the following 
-    // (table of computer choices vs player, and winner)
-    // PS: R P S R P S R P S
-    // CC: R R R P P P S S S
-    //  W: T W L L T W W L T
-    //sample string to declare winner: 
-    //"You Lose! Paper beats Rock
-    //SWITCH STATEMENT DOES  NOT WORK, IF/ELSE BETTER
-
 
 function playRound(playerSelection, computerSelection){
-  if ((playerSelection === "Rock" && computerSelection === "Rock") || 
-  (playerSelection === "Scissors" && computerSelection === "Scissors") ||
-  (playerSelection === "Paper" && computerSelection === "Paper")
-  ) {
+  if (playerSelection === computerSelection) {
     return "You tie! " + playerSelection + " ties with " + computerSelection + "!"
   } else if ((playerSelection === "Rock" && computerSelection === "Scissors") || 
   (playerSelection === "Scissors" && computerSelection === "Paper") ||
@@ -57,8 +24,37 @@ function playRound(playerSelection, computerSelection){
     return "You lose! " + computerSelection + " beats " + playerSelection + "!";
   }
 }
-console.log(playRound(playerSelection,computerSelection))
 
+function game(){
+  for (let i = 0; i < 5; i++) {
+    let playerString  = prompt("Choose Rock, Paper, or Scissors","")
+    const playerSelection = playerString[0].toUpperCase() + playerString.slice(1).toLowerCase();
+    const computerSelection = getComputerChoice();
+    console.log(playRound(playerSelection,computerSelection));
+  }
+}
+game();
+
+
+
+//PSEUDOCODE BELOW:
+//1. getComputerChoice
+  //Math.floor(Math.random() * 3) + 1;
+  //if 1 then CC is Rock
+  //2 '' Paper
+  //3 '' Scissors
+//2. Get playerSelection: prompt user "choose Rock paper or scissors"
+  //use prompt()
+  //make case insensitive
+//3. function playRound(playerSelection, computerSelection){}
+  //switch statement of the following 
+    // (table of computer choices vs player, and winner)
+    // PS: R P S R P S R P S
+    // CC: R R R P P P S S S
+    //  W: T W L L T W W L T
+    //sample string to declare winner: 
+    //"You Lose! Paper beats Rock
+    //SWITCH STATEMENT DOES  NOT WORK, IF/ELSE BETTER
 //4. function game()
     //simply function playRound done 5 times?
     //use console.log() to display the results of each round and winner in the end
